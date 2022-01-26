d78d3bfd04ab0383e324931f6b215071af69a61d
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2022-01-26 15:09:49 -0500

    completed exercises

---
+++ exercises/CircleExercise.java
@@ -0,0 +1,9 @@
+public class CircleExercise {
+    public static void main(String[] args) {
+        System.out.print("Perimeter is = ");
+        System.out.println(2 * 3.14159 * 7.5);
+        System.out.print("Area is = ");
+        System.out.println(3.14159 * 7.5 * 7.5);
+    }
+    
+}
---
+++ exercises/PrintEquations.java
@@ -0,0 +1,14 @@
+public class PrintEquations{
+    public static void main(String[] args) {
+        System.out.print("125 + 24 = ");
+        System.out.println(125 + 24);
+        System.out.print("125 - 24 = ");
+        System.out.println(125 - 24);
+        System.out.print("125 x 24 = ");
+        System.out.println(125 * 24);
+        System.out.print("125 / 24 = ");
+        System.out.println(125 / 24);
+        System.out.print("125 mod 24 = ");
+        System.out.println(125 % 24);
+    }
+}
