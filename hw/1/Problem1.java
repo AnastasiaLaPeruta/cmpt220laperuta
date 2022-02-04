5a98fd28e63e5a511bd2469a46f230f677c6f6df
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2022-02-04 09:40:12 -0500

    fixed old problem and finished hw 1

---
+++ hw/1/Problem1.java
@@ -0,0 +1,6 @@
+import java.lang.Math;
+public class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("The discriminant of the equation 2x^2 + 5x + 4 = 0 is " + (Math.pow(5,2) - 4*2*4));
+    }
+}
---
+++ hw/1/Problem2.java
@@ -0,0 +1,6 @@
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println("When starting velocity is 4.6m/sec, ending velocity is 11.5m/sec and time span is .6sec, ");
+        System.out.println("the average acceleration is " + ((11.5 - 4.6)/0.6));
+    }
+}
