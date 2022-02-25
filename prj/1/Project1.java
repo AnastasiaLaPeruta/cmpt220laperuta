e21a3032aa24e0a8be6fec890eb668f5e5be8bfd
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2022-02-25 09:49:28 -0500

    completed project 1

--- prj/1/Project1.java
+++ prj/1/Project1.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Project1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter S1 and T as two integers seperated by a space: ");
+        System.out.print("Enter S1 and T as two integers seperated by a space (must be between -1000 and 1000): ");
         int s1 = input.nextInt();
         int t = input.nextInt();
-        System.out.println(s1 + " " + t);
+        System.out.println(t * 2 - s1);
         input.close();
     }
     
