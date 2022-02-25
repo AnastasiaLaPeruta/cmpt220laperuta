af6600139707c2bb3526579106ceae733fc16f8c
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2022-02-25 10:19:42 -0500

    completed prob 1 of hw

--- hw/4/ProbNum1.java
+++ hw/4/ProbNum1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class ProbNum1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(Systme.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of sides: ");
         double n = input.nextDouble();
         System.out.print("Enter the side length: ");
