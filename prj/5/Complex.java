8c13e3ddfb71af2824291e22e086e9ea6ad0e85d
Juan Arias <juan.arias@marist.edu>
2022-05-14 12:12:20 -0400

    Feedback

--- prj/5/Complex.java
+++ prj/5/Complex.java
@@ -1,108 +1,130 @@
-public class Complex extends Number implements Cloneable, Comparable<Complex>{
-  //data fields
+public class Complex extends Number implements Cloneable, Comparable<Complex> {
+  // data fields
   private double a;
   private double b;
-  //Constructors
-  Complex(){
+
+  // Constructors
+  Complex() {
     a = 0;
     b = 0;
   }
-  Complex(double a, double b){
+
+  Complex(double a, double b) {
     this.a = a;
     this.b = b;
   }
-  Complex(double a){
+
+  Complex(double a) {
     this.a = a;
     b = 0;
   }
 
-  //getters and setters
-  public double getA(){
+  // getters and setters
+  public double getA() {
     return a;
   }
-  public void setA(double a){
+
+  public void setA(double a) {
     this.a = a;
   }
-  public double getB(){
+
+  public double getB() {
     return b;
   }
-  public void setB(double b){
+
+  public void setB(double b) {
     this.b = b;
   }
-  //adds the complex numbers
-  public static Complex add(Complex firstCom, Complex secondCom){
-    double first = firstCom.getA() + secondCom.getA(); 
-    double second = firstCom.getB() + secondCom.getB();
-    return new Complex(first,second);   
+
+  // adds the complex numbers
+  public Complex add(Complex secondCom) {
+    double first = getA() + secondCom.getA();
+    double second = getB() + secondCom.getB();
+    return new Complex(first, second);
   }
-  //subtracts the complex numbers
-  public static Complex subtract(Complex firstCom, Complex secondCom){
-    double first = firstCom.getA() - secondCom.getA();
-    double second = firstCom.getB() - secondCom.getB();
-    return new Complex(first,second);
+
+  // subtracts the complex numbers
+  public Complex subtract(Complex secondCom) {
+    double first = getA() - secondCom.getA();
+    double second = getB() - secondCom.getB();
+    return new Complex(first, second);
   }
-  //multiplies the complex numbers
-  public static Complex multiply(Complex firstCom, Complex secondCom){
-    double first = (firstCom.getA()*secondCom.getA())-(firstCom.getB()*secondCom.getB());
-    double second = (firstCom.getB()*secondCom.getA())+(firstCom.getA()*secondCom.getB());
-    return new Complex(first,second);
+
+  // multiplies the complex numbers
+  public Complex multiply(Complex secondCom) {
+    double first = (getA() * secondCom.getA()) - (getB() * secondCom.getB());
+    double second = (getB() * secondCom.getA()) + (getA() * secondCom.getB());
+    return new Complex(first, second);
   }
-  //divides the complex numbers
-  public static Complex divide(Complex firstCom, Complex secondCom){
-    double first = ((firstCom.getA()*secondCom.getA()+firstCom.getB()*secondCom.getB())/(secondCom.getA()*secondCom.getA()+secondCom.getB()*secondCom.getB()));
-    double second = ((firstCom.getB()*secondCom.getA()-firstCom.getA()*secondCom.getB())/(secondCom.getA()*secondCom.getA()+secondCom.getB()*secondCom.getB()));
-    return new Complex(first,second);
+
+  // divides the complex numbers
+  public Complex divide(Complex secondCom) {
+    double first = ((getA() * secondCom.getA() + getB() * secondCom.getB())
+        / (getA() * secondCom.getA() + secondCom.getB() * secondCom.getB()));
+    double second = ((getB() * secondCom.getA() - getA() * secondCom.getB())
+        / (secondCom.getA() * secondCom.getA() + secondCom.getB() * secondCom.getB()));
+    return new Complex(first, second);
   }
-  //gets the absolute value of a complex number
-  public static double abs(Complex num){
-    return Math.sqrt(num.getA()*num.getA()+num.getB()*num.getB());
+
+  // gets the absolute value of a complex number
+  public double abs() {
+    return Math.sqrt(getA() * getA() + getB() * getB());
   }
-  //return the real part of a complex number
+
+  // return the real part of a complex number
   public double getRealPart() {
     return a;
   }
-  //return the imaginary part of a complex number
-  public double getImaginaryPart(){
+
+  // return the imaginary part of a complex number
+  public double getImaginaryPart() {
     return b;
   }
-  //turns it into string
+
+  // turns it into string
   @Override
-  public String toString(){
-    return (b==0? "" + a : a + " + " + b + "i"); //returns string, if b = 0 returns just "a"
+  public String toString() {
+    return (b == 0 ? "" + a : a + " + " + b + "i"); // returns string, if b = 0 returns just "a"
   }
-  //implements these because they are abstract in parent class, changes abs value to different types
+
+  // implements these because they are abstract in parent class, changes abs value
+  // to different types
   @Override
-  public int intValue(){
-    return (int) Complex.abs(new Complex(getA(),getB()));
+  public int intValue() {
+    return (int) Complex.abs(new Complex(getA(), getB()));
   }
+
   @Override
-  public double doubleValue(){
-    return (double) Complex.abs(new Complex(getA(),getB()));
+  public double doubleValue() {
+    return (double) Complex.abs(new Complex(getA(), getB()));
   }
+
   @Override
-  public  float floatValue(){
-    return (float) Complex.abs(new Complex(getA(),getB()));
+  public float floatValue() {
+    return (float) Complex.abs(new Complex(getA(), getB()));
   }
+
   @Override
-  public long longValue(){
-    return (long) Complex.abs(new Complex(getA(),getB()));
+  public long longValue() {
+    return (long) Complex.abs(new Complex(getA(), getB()));
   }
-  //overrides compareTo method from Comparable and compares the abs value
+
+  // overrides compareTo method from Comparable and compares the abs value
   @Override
-  public int compareTo(Complex o){
-    if (Complex.abs(new Complex(getA(),getB())) > Complex.abs(o))
+  public int compareTo(Complex o) {
+    if (abs() > o.abs())
       return 1;
-    else if (Complex.abs(new Complex(getA(),getB())) < Complex.abs(o))
+    else if (abs() < o.abs())
       return -1;
-    else 
+    else
       return 0;
   }
+
   @Override
-  public Object clone(){
-    try{
+  public Object clone() {
+    try {
       return super.clone();
-    }
-    catch(CloneNotSupportedException ex){
+    } catch (CloneNotSupportedException ex) {
       return null;
     }
   }
--- prj/5/Project5.java
+++ prj/5/Project5.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
+
 public class Project5 {
-  public static void main(String[] args) {
-    //gets numbers from user
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the first complex number: ");
-    double a = input.nextDouble();
-    double b = input.nextDouble();
-    //creates instance of Complex class using input
-    Complex first = new Complex(a,b);
-    //gets numbers for second complex number
-    System.out.print("Enter the second complex number: ");
-    double secondA = input.nextDouble();
-    double secondB = input.nextDouble();
-    //creates a second instance
-    Complex second = new Complex(secondA,secondB);
-    input.close();
-    //displays the results of adding them together
-    System.out.println("(" + first.toString() + ") + (" + second.toString() + ") = " +
-       (Complex.add(first,second)).toString());
-    //displays the results of subtracting them
-    System.out.println("(" + first.toString() + ") - (" + second.toString() + ") = " +
-       (Complex.subtract(first,second)).toString());
-    //displays the results of multiplying them together
-    System.out.println("(" + first.toString() + ") * (" + second.toString() + ") = " +
-       (Complex.multiply(first,second)).toString());
-    //displays the results of dividing them
-    System.out.println("(" + first.toString() + ") / (" + second.toString() + ") = " +
-       (Complex.divide(first,second)).toString());
-    //displays the results of the absolute value of the first complex number
-    System.out.println("|" + first.toString() + "| = " + Complex.abs(first));
-  } 
+   public static void main(String[] args) {
+      // gets numbers from user
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first complex number: ");
+      double a = input.nextDouble();
+      double b = input.nextDouble();
+      // creates instance of Complex class using input
+      Complex first = new Complex(a, b);
+      // gets numbers for second complex number
+      System.out.print("Enter the second complex number: ");
+      double secondA = input.nextDouble();
+      double secondB = input.nextDouble();
+      // creates a second instance
+      Complex second = new Complex(secondA, secondB);
+      input.close();
+      // displays the results of adding them together
+      System.out.println("(" + first.toString() + ") + (" + second.toString() + ") = " +
+            (first.add(second)).toString());
+      // displays the results of subtracting them
+      System.out.println("(" + first.toString() + ") - (" + second.toString() + ") = " +
+            (first.subtract(second)).toString());
+      // displays the results of multiplying them together
+      System.out.println("(" + first.toString() + ") * (" + second.toString() + ") = " +
+            (first.multiply(second)).toString());
+      // displays the results of dividing them
+      System.out.println("(" + first.toString() + ") / (" + second.toString() + ") = " +
+            (first.divide(second)).toString());
+      // displays the results of the absolute value of the first complex number
+      System.out.println("|" + first.toString() + "| = " + first.abs());
+   }
 }
